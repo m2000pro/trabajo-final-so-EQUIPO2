cac5f0c8dcb0cc95e028ca8671e1a3a98dd5359b
Miguel Selva <ssmjesus0705@gmail.com>
2026-07-25 08:15:58 +0000

    Agregacion de FLASK-CORS

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,10 +4,13 @@ from .config import Config
 from .errors import register_error_handlers
 from .extensions import db
 from .routes import api_bp
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     app.config.from_object(Config)
 
